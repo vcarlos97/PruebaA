0bcefe93eb23cb4d45a0032ced5bcf9dfd80e5db
irega97 <ivanreq97@gmail.com>
2020-06-10 18:36:49 +0200

    SplashScreen

--- app/src/main/java/edu/upc/login/MainActivity.java
+++ app/src/main/java/edu/upc/login/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         //Creamos interceptor
